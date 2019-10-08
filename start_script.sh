b11b8329b8b43be2e92587cf3423252efecfb73b
Никита Никифоров <s02170179@practicum.cs.msu.su>
2019-10-08 09:12:20 +0300

    change email in git config

--- start_script.sh
+++ start_script.sh
@@ -4,7 +4,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
 vim some_file
 
-git config --global user.email "s02170179@practicum.cs.msu.su"
+git config --global user.email "nickiforov.nik@gmail.com"
 git config --global user.name "Никита Никифоров"
 
 
