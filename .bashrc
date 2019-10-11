d376336d10eb9c5509922259ddbc0eed0b0100db
Никита Никифоров <nickiforov.nik@gmail.com>
2019-10-11 17:33:50 +0300

    Adding to the initial bash config

---
+++ .bashrc
@@ -0,0 +1,4 @@
+PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
+
+# added by Miniconda3 installer
+#export PATH="/home/balda/miniconda3/bin:$PATH"
--- start_script.sh
+++ start_script.sh
@@ -2,6 +2,9 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
+
+cat .bashrc >>  ~/.bashrc
+
 vim some_file
 
 git config --global user.email "nickiforov.nik@gmail.com"
