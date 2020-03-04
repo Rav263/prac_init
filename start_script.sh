93bf4661696f61c338ddf52ca8b1df93f4b1f355
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-04 12:10:42 +0300

    Update for alt systems

--- start_script.sh
+++ start_script.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+mkdir ~/.vim/
+mkdir ~/.vim/backup
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
